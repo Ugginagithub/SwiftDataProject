c8d00fa0b85119b7efd35a2ed26f25ffc9fe3945
UGGINA TARUN PAVAN KUMAR <98165814+Ugginagithub@users.noreply.github.com>
2026-06-24 10:47:19 +0530

    Deeper learning about SwiftData
    Deeper learning about SwiftData

---
+++ SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Tarun on 24/06/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
